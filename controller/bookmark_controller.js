8113a1a202b7122188950e07d7aa9856818767b9
hritikadhikari05 <hritikadhikari05@gmail.com>
2023-08-22 11:36:21 +0545

    WIP in remove bookmark

--- controller/bookmark_controller.js
+++ controller/bookmark_controller.js
@@ -29,3 +29,30 @@ exports.addBookmark = async (req, res) => {
     });
   }
 };
+
+/* Remove selected post from the bookmark */
+exports.removeBookmark = async (req, res) => {
+  const { postId } = req.params;
+  const { userId } = req.user;
+
+  try {
+    const bookmarks =
+      await Bookmark.findOneAndDelete({
+        userId: userId,
+        postId: postId,
+      });
+    if (!bookmarks) {
+      return res.status(400).json({
+        message:
+          "You have not bookmarked this post",
+      });
+    }
+    return res.status(200).json({
+      message: "Post removed from bookmark",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+};
