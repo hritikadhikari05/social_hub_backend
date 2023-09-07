24e6fda654122cdffc626c441db3f53afe9f7504
Hritik <hritikadhikari05@gmail.com>
2023-09-07 11:31:24 +0545

    added port to env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const dbConnect = require("./db_connection/connection");
 const app = require("./app");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 dbConnect();
 
